cd77832dd39cb590249fbf46fd82cfbbb574c196
Damian <damian.github@gmail.com>
2021-11-13 14:30:57 +0100

    fixed mobile whitespace, change popup styles

--- assets/js/index-animations.js
+++ assets/js/index-animations.js
@@ -1,3 +1,4 @@
 AOS.init({
     duration: 500,
+    once: true, // animation happen only once - while scrolling down
 });
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -3,6 +3,6 @@ const popup = document.querySelector('.popup');
 const close = document.querySelector('.popup__close');
 
 close.addEventListener('click', () => {
-    overlay.style.visibility = 'hidden';
-    popup.style.visibility = 'hidden';
+    overlay.style.display = 'none';
+    popup.style.display = 'none';
 });
